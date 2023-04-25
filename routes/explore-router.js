f9ef69a5c4c29e14947a9ed4eb8ef92fbbf95cb6
Oleksandr Levchuk <73852716+alxlvgit@users.noreply.github.com>
2023-04-24 21:02:38 -0700

    add .env example file

--- routes/explore-router.js
+++ routes/explore-router.js
@@ -5,25 +5,25 @@ const prisma = new PrismaClient();
 router.get('/', async (req, res) => {
     const users = await prisma.user.findMany();
     console.log(users, "test DB");
-    res.render('./explore', { users: users });
+    res.render('./explore-views/explore', { users: users });
 }
 );
 
 // Events page 
 router.get('/events', async (req, res) => {
-    res.render('./explore-events');
+    res.render('./explore-views/explore-events');
 }
 );
 
 // Feeds page
 router.get('/feeds', async (req, res) => {
-    res.render('./explore-feeds');
+    res.render('./explore-views/explore-feeds');
 }
 );
 
 // Feeds post page
 router.get('/feeds/:id', async (req, res) => {
-    res.render('./feeds-post');
+    res.render('./explore-views/feeds-post');
 }
 );
 
