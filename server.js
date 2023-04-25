8031e7f00d7a760c53f8af45c6e7180108b83fff
Oleksandr Levchuk <73852716+alxlvgit@users.noreply.github.com>
2023-04-25 11:35:39 -0700

    render 5 recommended local events

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config()
 const port = process.env.PORT || 3000;
 const exploreRouter = require('./routes/explore-router');
 
@@ -8,6 +9,13 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use('/', exploreRouter);
 app.use(express.static(__dirname + "/static"));
+
+app.get("/secretKeys", (req, res) => {
+    const secrets = {
+        TICKETMASTER_API_KEY: `${process.env.TICKETMASTER_API_KEY}`
+    }
+    res.status(200).json(JSON.stringify(secrets));
+});
 
 
 app.listen(port, () => {
