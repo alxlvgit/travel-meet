cea529e07316a5fba3cb573f3ebd3e1c9f05533d
Oleksandr Levchuk <73852716+alxlvgit@users.noreply.github.com>
2023-04-28 00:36:48 -0700

    sort events buttons are functional

--- static/feeds.js
+++ static/feeds.js
@@ -2,6 +2,11 @@ const feedsButton = document.getElementById("feed-link");
 
 feedsButton.addEventListener('click', () => {
     container.innerHTML = "";
+    sortEventsButtons.forEach(btn => {
+        btn.classList.remove('active');
+    });
+    feedsButton.classList.add('active');
+    eventsButton.classList.remove('active');
     const testText = document.createElement('p');
     testText.textContent = "This is a test";
     container.appendChild(testText);
