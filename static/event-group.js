a18cf0dfd32b87d05505bdf8c135386b044f1b6f
Brian Xu <mattx0121@gmail.com>
2023-05-10 13:45:51 -0700

    make all the avatars can be clickable and show the profile page

--- static/event-group.js
+++ static/event-group.js
@@ -39,7 +39,8 @@ if (groupRelatedPageOpen) {
 // Generate group member HTML
 function generateMemberHtml(member) {
     return `
-      <div class="swiper-slide w-auto">
+      <div class="swiper-slide w-auto relative">
+      <a href="/user-profile/${member.id}" class="absolute w-full h-full top-0 left-0 z-20"></a>
         <img class="w-16 h-16 object-cover rounded-full border-2 border-white outline-[#878d26] outline outline-2 m-1"
           src="${member.profileImageURI}" alt="Profile 1" />
       </div>
