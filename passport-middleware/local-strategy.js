fb6d605f11a621c629584166bce8ecf982c5d3c5
Oleksandr Levchuk <73852716+alxlvgit@users.noreply.github.com>
2023-06-01 15:53:55 -0700

    encrypt password

--- passport-middleware/local-strategy.js
+++ passport-middleware/local-strategy.js
@@ -2,31 +2,65 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const bcrypt = require('bcrypt');
 
+// This function is used by passport.authenticate to get the user from the database
+async function findUserByEmailAndPassword(email, password) {
+    const userFoundByEmail = await findUserByEmail(email);
+    if (!userFoundByEmail) {
+        return null;
+    }
+    const bcryptPrefix = /^\$2b\$/;
+    const passwordIsHashed = bcryptPrefix.test(userFoundByEmail.password);
+    // For older test accounts, the password is not hashed, so we need to compare the plain text password
+    let passwordVerified = passwordIsHashed ? await bcrypt.compare(password, userFoundByEmail.password) : password === userFoundByEmail.password;
+    if (!passwordVerified) {
+        return null;
+    }
+    return userFoundByEmail;
+}
+
+// This function is used by findUserByEmailAndPassword to get the user from the database
+async function findUserByEmail(email) {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email,
+            },
+        });
+        if (!user) {
+            return null;
+        }
+        return user;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// This function is used by passport.deserializeUser to get the user from the database
+async function getUserById(id) {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: Number(id),
+            },
+        });
+        return user;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Passport local strategy
 const localStrategy = new LocalStrategy(
     {
         usernameField: "email",
         passwordField: "password",
     },
     async (email, password, done) => {
-        async function getUserByEmail(email) {
-            try {
-                const user = await prisma.user.findUnique({
-                    where: {
-                        email: email,
-                    },
-                });
-                return user;
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        const user = await getUserByEmail(email);
+        const user = await findUserByEmailAndPassword(email, password);
         if (!user) {
-            return done(null, false, { message: "Incorrect email." });
-        }
-        if (user.password !== password) {
-            return done(null, false, { message: "Incorrect password." });
+            return done(null, false, { message: "Incorrect email or password." });
         }
         return done(null, user);
     }
@@ -37,18 +71,6 @@ passport.serializeUser(function (user, done) {
 });
 
 passport.deserializeUser(async function (id, done) {
-    async function getUserById(id) {
-        try {
-            const user = await prisma.user.findUnique({
-                where: {
-                    id: Number(id),
-                },
-            });
-            return user;
-        } catch (error) {
-            console.log(error);
-        }
-    }
     const user = await getUserById(id);
     done(null, user);
 });
